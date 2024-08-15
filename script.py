7de8f8241c323b20c5b14e831c3f9ba554f7ec67
Nico Tapiero <47359303+nicotapiero@users.noreply.github.com>
2024-08-15 08:32:35 -0400

    quit early if already seen

--- script.py
+++ script.py
@@ -11,6 +11,9 @@ import json
 driver = webdriver.Firefox()
 driver.get("https://challenge-jlpt.com/")
 
+new_questions = 0
+num_remaining = None
+
 for i in range(0, 300):
 
     # firefox_profile = webdriver.FirefoxProfile()
@@ -21,9 +24,10 @@ for i in range(0, 300):
 
 
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    # driver.execute_script('document.getElementById("08_n5grammar.csv").checked = true ')
+    driver.execute_script('document.getElementById("10_b_kanji_yomi.csv").checked = true ')
 
-    # 
-    driver.execute_script('document.getElementById("08_n5grammar.csv").checked = true ')
+   
     # driver.execute_script('document.getElementById("09_n5vocabulary.csv").checked = true ')
 
 
@@ -42,7 +46,23 @@ for i in range(0, 300):
         answer = driver.execute_script(f"""return document.getElementsByClassName("grid")[0].getElementsByClassName("normal")[0].getElementsByTagName("button")[{j}].innerHTML""")
         answers.append(answer)
 
-    id = driver.execute_script("""return document.getElementsByTagName("span")[6].textContent""")
+    id = driver.execute_script("""return [...document.getElementsByTagName("span")].filter(span => {return span.innerHTML.includes("問題ID") ? span : null})[0].textContent""")
+    id = id.strip()
+
+    full_text = ""
+    with open("questions.txt", "r") as f:
+        full_text = f.read()
+
+    if id in full_text:
+        print("duplicate")
+        driver.execute_script("""window.location.replace("https://challenge-jlpt.com/")""")
+        time.sleep(4)
+        continue
+    else:
+        print('NEW!!!')
+        new_questions += 1
+
+
 
     correct_index = -1
     for j in range(4):
@@ -62,7 +82,6 @@ for i in range(0, 300):
         break
 
     result = result.strip()
-    id = id.strip()
 
     print(result, answers,id )
 
@@ -91,27 +110,19 @@ for i in range(0, 300):
 
     # print(str(obj))
 
-    full_text = ""
-    with open("questions.txt", "r") as f:
-        full_text = f.read()
-
     f = open("questions.txt", "a")
-
-
     f.write(string)
-
-
     f.close()
 
    
 
+    
+    print(f"scanned/saved: {i+1}, new questions: {new_questions}")
 
-
-    print(f"saved {i}")
-    if id in full_text:
-        print("duplicate")
-    else:
-        print('NEW!!!')
+    if num_remaining is not None:
+        if new_questions == num_remaining:
+            print('found them all!')
+            break
 
     # N5V0041 - rb in answers
 
@@ -121,5 +132,5 @@ for i in range(0, 300):
 
 
     
-    # driver.close()
-    # driver.quit()
+driver.close()
+driver.quit()
